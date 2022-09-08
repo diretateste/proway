338ebac9cab457ac65c18ea6507c812e1e33d40e
Leandro Ruoff <leandroruoff@yahoo.com.br>
2022-09-08 17:55:58 -0300

    O que faltou antes

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'ATz63HHRJQbSHaSRh_Jnu3pWXncui7us',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
