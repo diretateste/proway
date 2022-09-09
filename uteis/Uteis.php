acc420e28ce798ea64cb5b58779d0d7ac52ad581
Leandro Ruoff <leandroruoff@yahoo.com.br>
2022-09-08 21:05:36 -0300

    uteis

---
+++ uteis/Uteis.php
@@ -0,0 +1,81 @@
+<?php
+namespace app\uteis;
+
+class Uteis{
+
+    public static function getStatus(){
+        return ['Inativo', 'Ativo'];     
+    }      
+
+    public static function onStatus($st){         
+        $status = ['Inativo', 'Ativo'];         
+        return $status[$st];    
+    }
+
+    public static function getUf($sig){
+        $estados = [
+            "AC" => "Acre", 
+            "AL" => "Alagoas", 
+            "AM" => "Amazonas", 
+            "AP" => "Amapá",
+            "BA" => "Bahia",
+            "CE" => "Ceará",
+            "DF" => "Distrito Federal",
+            "ES" => "Espírito Santo",
+            "GO" => "Goiás",
+            "MA" => "Maranhão",
+            "MT" => "Mato Grosso",
+            "MS" => "Mato Grosso do Sul",
+            "MG" => "Minas Gerais",
+            "PA" => "Pará",
+            "PB" => "Paraíba",
+            "PR" => "Paraná",
+            "PE" => "Pernambuco",
+            "PI" => "Piauí",
+            "RJ" => "Rio de Janeiro",
+            "RN" => "Rio Grande do Norte",
+            "RO" => "Rondônia",
+            "RS" => "Rio Grande do Sul",
+            "RR" => "Roraima",
+            "SC" => "Santa Catarina",
+            "SE" => "Sergipe",
+            "SP" => "São Paulo",
+            "TO" => "Tocantins"            
+        ];
+
+        return $estados[$sig];
+    }
+
+    public static function getEstados(){
+        return [
+            "AC" => "Acre", 
+            "AL" => "Alagoas", 
+            "AM" => "Amazonas", 
+            "AP" => "Amapá",
+            "BA" => "Bahia",
+            "CE" => "Ceará",
+            "DF" => "Distrito Federal",
+            "ES" => "Espírito Santo",
+            "GO" => "Goiás",
+            "MA" => "Maranhão",
+            "MT" => "Mato Grosso",
+            "MS" => "Mato Grosso do Sul",
+            "MG" => "Minas Gerais",
+            "PA" => "Pará",
+            "PB" => "Paraíba",
+            "PR" => "Paraná",
+            "PE" => "Pernambuco",
+            "PI" => "Piauí",
+            "RJ" => "Rio de Janeiro",
+            "RN" => "Rio Grande do Norte",
+            "RO" => "Rondônia",
+            "RS" => "Rio Grande do Sul",
+            "RR" => "Roraima",
+            "SC" => "Santa Catarina",
+            "SE" => "Sergipe",
+            "SP" => "São Paulo",
+            "TO" => "Tocantins"              
+        ];
+    }
+}
+?>
